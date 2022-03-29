1f58b27705b43073a94217fda83ad44d8b6bc0bc
PradoCarretero <p.carreteroayala@gmail.com>
2022-03-29 09:19:05 +0200

    randomnumber

--- js/main.js
+++ js/main.js
@@ -2,4 +2,29 @@
 /* VARIABLES */
 const select = document.querySelector(".js-select");
 const amount = document.querySelector(".js-amount");
-const amount = document.querySelector(".js-button");
+const button = document.querySelector(".js-button");
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+const randomNumber = getRandomNumber(6);
+    console.log(randomNumber);
+    
+
+
+    if (select.value===randomNumber) {
+        console.log("has ganado")
+    }
+    else {
+        console.log("has perdido :");
+    }
+
+/* function handleClickButton(event) {
+    event.preventDefault();    
+    refreshface();
+    changeColor();
+}
+ */
+/* button.addEventListener("click", handleClickButton); */
