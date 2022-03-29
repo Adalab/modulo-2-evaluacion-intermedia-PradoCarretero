dc8988284670f93fa0bd4bd6ca85e59152f9b9ac
PradoCarretero <p.carreteroayala@gmail.com>
2022-03-29 09:39:47 +0200

    compared done

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
 "use strict";
 /* VARIABLES */
-const select = document.querySelector(".js-select");
+const selected = document.querySelector(".js-selected");
 const amount = document.querySelector(".js-amount");
 const button = document.querySelector(".js-button");
+const message = document.querySelector(".js-message");
 
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-const randomNumber = getRandomNumber(6);
+
+
+
+
+function compare () {
+    const randomNumber = getRandomNumber(6);
     console.log(randomNumber);
-    
-
-
-    if (select.value===randomNumber) {
+    const selectedvalue = selected.value;
+    console.log(selectedvalue);
+    if (selectedvalue==randomNumber) {
         console.log("has ganado")
+        message.innerHTML = 'Has ganado el doble de lo apostado';
     }
     else {
         console.log("has perdido :");
+        message.innerHTML = 'Has perdido lo apostado';
     }
-
-/* function handleClickButton(event) {
-    event.preventDefault();    
-    refreshface();
-    changeColor();
 }
- */
-/* button.addEventListener("click", handleClickButton); */
+
+function handleClickButton(event) {
+    event.preventDefault();    
+    compare();
+}
+
+button.addEventListener("click", handleClickButton);
