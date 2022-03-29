9ee0932672034c99dfefe0fdb70fd60150015702
PradoCarretero <p.carreteroayala@gmail.com>
2022-03-29 09:02:24 +0200

    create skeleton

---
+++ js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+/* VARIABLES */
+const select = document.querySelector(".js-select");
+const amount = document.querySelector(".js-amount");
+const amount = document.querySelector(".js-button");
