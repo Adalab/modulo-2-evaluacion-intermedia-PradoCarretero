a56e69df610f4ea9d4794e2cf574293abe7bb1c6
PradoCarretero <p.carreteroayala@gmail.com>
2022-04-06 19:52:31 +0200

    fix exercise after indications

--- js/main.js
+++ js/main.js
@@ -1,74 +1,83 @@
 "use strict";
 //--------------------VARIABLES
-const selected = document.querySelector(".js-selected");
-const amount = document.querySelector(".js-amount");
-const button = document.querySelector(".js-button");
+const selectedNumber = document.querySelector(".js-selected");
+const amountContainer = document.querySelector(".js-amount");
+const buttonPlay = document.querySelector(".js-button");
 const buttonRe = document.querySelector(".js-buttonRe");
-const message = document.querySelector(".js-message");
+const messageElement = document.querySelector(".js-message");
 const totaltext = document.querySelector(".js-totaltext");
 let pocket = 50;
 
 //--------------------FUNCTIONS
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
 
-function resetcounter(){
-    pocket = 50;
+function resetcounter() {
+  pocket = 50;
+
+  const messageStart = "Vamos a jugar!";
+  renderMessage(messageStart);
+  totaltext.innerHTML = "Saldo: 50";
+  selectedNumber.value = 1;
 }
-function hidden (){
-    button.classList.remove("hidden");
-        buttonRe.classList.add("hidden");
+function hidden() {
+  buttonPlay.classList.remove("hidden");
+  buttonRe.classList.add("hidden");
 }
-function finish (){
-    if (pocket>200){
-        button.classList.add("hidden");
-        buttonRe.classList.remove("hidden");
-        message.innerHTML= '¡Has ganado!';
-    }
-    else if (pocket<0){
-        message.innerHTML= '¡Has perdido!';
-    }
+function finish() {
+  if (pocket <= 0 || pocket >= 200) {
+    buttonPlay.classList.add("hidden");
+    buttonRe.classList.remove("hidden");
+  } else if (pocket >= 200) {
+    const messageEndWin = "¡Has ganado!";
+    renderMessage(messageEndWin);
+  }
 }
 
-function accumulated(randomNumber, selectedvalue, pocket) {
-    const aggregate = amount.value;
-    if (selectedvalue==randomNumber) {
-     pocket += 2 * parseInt(aggregate);
-        totaltext.innerHTML= `Saldo ${pocket} `;
-        
-        return pocket;
+function accumulated(randomNumber, selectedvalue) {
+  if (amountContainer.value > pocket) {
+    const messageErr = `No puedes apostar más de lo que tienes `;
+    renderMessage(messageErr);
+  } else {
+    const aggregate = amountContainer.value;
+    if (selectedvalue == randomNumber) {
+      pocket = 2 * parseInt(aggregate) + pocket;
+    } else {
+      pocket = pocket - parseInt(aggregate);
     }
-    else {
-        pocket -= parseInt(aggregate);
-        totaltext.innerHTML= `Saldo ${pocket} `;
-        return pocket; 
-    }
+    totaltext.innerHTML = `Saldo ${pocket} `;
+  }
 }
 
-function compare (randomNumber, selectedvalue) {
-    if (selectedvalue==randomNumber) {
-        message.innerHTML = 'Has ganado el doble de lo apostado';
-    }
-    else {
-        message.innerHTML = 'Has perdido lo apostado';
-    }
+function compare(randomNumber, selectedvalue) {
+  if (selectedvalue === randomNumber) {
+    const messageWin = "Has ganado el doble de lo apostado :D";
+    renderMessage(messageWin);
+  } else {
+    const messageLost = "Has perdido lo apostado :(";
+    renderMessage(messageLost);
+  }
 }
-//--------------------MAIN FUNCTION 
-function handleClickButton(event) {
-    event.preventDefault();
-    const randomNumber = getRandomNumber(6);
-    const selectedvalue = selected.value;
-    compare(randomNumber, selectedvalue);
-    accumulated(randomNumber, selectedvalue, pocket);
-   /*  finish(); */
+
+function renderMessage(value) {
+  messageElement.innerHTML = value;
 }
-/* function handleClickButtonRe(event) {
-    event.preventDefault();  
-    hidden();
-    resetcounter();
-} */
+//--------------------MAIN FUNCTION
+function handleClickButtonPlay(event) {
+  event.preventDefault();
+  const randomNumber = getRandomNumber(6);
+  const selectedvalue = parseInt(selectedNumber.value);
+  compare(randomNumber, selectedvalue);
+  accumulated(randomNumber, selectedvalue);
+  finish();
+}
+function handleClickButtonRe(event) {
+  event.preventDefault();
+  hidden();
+  resetcounter();
+}
 
 //--------------------EVENT LISTENER
-button.addEventListener("click", handleClickButton);
-/* buttonRe.addEventListener("click", handleClickButtonRe); */
+buttonPlay.addEventListener("click", handleClickButtonPlay);
+buttonRe.addEventListener("click", handleClickButtonRe);
